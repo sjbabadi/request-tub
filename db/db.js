73b073c3d4e62a2af59ad6f9afdb310e7ad765e0
Melinda <melinda.hl.lim@gmail.com>
2021-03-10 10:13:56 -0500

    Connect postgres

---
+++ db/db.js
@@ -0,0 +1,12 @@
+const Pool = require("node-postgres").Pool;
+require("dotenv").config();
+
+const pool = new Pool({
+  user: process.env.PG_USER,
+  password: process.env.PG_PW,
+  host: "localhost",
+  port: 5432,
+  database: "request_bin"
+});
+
+module.exports = pool;
--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 let express = require('express')
 let app = express()
+const pool = require('./db/db')
 
-app.get('/', (req, res) => res.send("Hello!"))
+app.get('/', async (req, res) => {
+  try {
+    const allRequests = await pool.query("SELECT * FROM bins");
+    res.json(allRequests.rows);
+    console.log(allRequests);
+    // res.send(allRequests)
+  } catch (err) {
+    console.log(err.message);
+  }
+
+})
+
+/*
+Sheila post request sample 
+
+app.post("/todos", async (req, res) => {
+  try {
+    const { text } = req.body;
+    const newTodo = await pool.query("INSERT INTO todo (text) VALUES($1)", [text]);
+    res.json(newTodo.rows[0]);
+  } catch(err) {
+    console.error(err.message);
+  }
+});
+*/
 
 app.listen(4000, () => console.log("hi"))
