15d4a233a4d19a257523d686b499483fd1ae50d0
ChristopherDurand <chris@cdurand.net>
2021-03-12 17:01:50 -0600

    Button now disables

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import bins from '../services/bins'
 
 const Home = () => {
   const history = useHistory();
-
+  const [disabled, setDisabled] = useState(true);
+  
   const createBin = (e) => {
     e.preventDefault();
+    setDisabled(false);
     const uri = bins.create().then(uri => { 
       history.push(`/bin/${uri}`);
     });
@@ -18,7 +20,7 @@ const Home = () => {
       <p class="mt-8 inline-block ">Request Tub is a place where you can send your API calls or webhooks to gain valuable insight on their structure.</p>
       <p class="mt-8 inline-block ">Tubs are ephemeral and disappear after 48 hours, and hold only the latest 20 requests.</p>
       <p class="mt-8 inline-block ">Click "Create Tub" to get started!</p>
-      <button  class="mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onClick={createBin}>Create Tub</button>
+      <button disabled={disabled} class="mt-8 bg-blue-500 hover:bg-blue-700 disabled:bg-gray-300 disabled:border-gray-500 text-white font-bold py-2 px-4 border border-blue-700 rounded" onClick={createBin}>Create Tub</button>
     </div>
   )
 };
