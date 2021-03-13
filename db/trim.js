bba29bf282cdfdfbc87f7bf4d5deadf4c33f9ef0
ChristopherDurand <chris@cdurand.net>
2021-03-12 19:59:06 -0600

    Set up cron job trim files

---
+++ db/trim.js
@@ -0,0 +1,14 @@
+/*
+
+https://phoenixnap.com/kb/set-up-cron-job-linux
+
+Setting up the cron job
+1. chmod 755 trim
+2. crontab -e
+3. 0 * * * * /__path_to_trim__/trim
+
+*/
+
+const pool = require('./db/db');
+
+await pool.query("delete * from bins where created_at < now() - interval '2 days'")
